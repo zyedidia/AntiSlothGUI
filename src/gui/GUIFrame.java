95f15569545c25e7becf845f8d3267b332452dda
ChiyenLee <a.gorilla.john@gmail.com>
2014-04-03 15:08:05 -0400

    tabbed frame

--- src/gui/GUIFrame.java
+++ src/gui/GUIFrame.java
@@ -1,14 +1,48 @@
 package gui;
 
+import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTabbedPane;
 
 @SuppressWarnings("serial")
 public class GUIFrame extends JFrame {
 	public GUIFrame() {
 		super("Anti-Sloth");
+		super.setSize(400, 400);
+		
+		tab();
+		
+		
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
 	}
 	
-	public static void main(String[] args) {
+	public void tab() {
+		JTabbedPane tabbed = new JTabbedPane(JTabbedPane.TOP);
+		tabbed.setBounds(20,20, 360, 360);
+		getContentPane().add(tabbed);
+		
+		JComponent panel1 = new JTabbedPane();
+		tabbed.addTab("Test", null, panel1, "Does nothing");
+		
+		JComponent panel2 = new JTabbedPane();
+		tabbed.addTab("Test2", null, panel2, "Does nothing");
+		
+		JComponent label = new JLabel();
+		JComponent but = new JButton();		
+		panel1.add(label);
+		panel1.add(but);
+	}
+	
+	public void add (JComponent Panel, JComponent Comp) {
+		
+	}
+	
+	public static void main(String [] args) {
+		GUIFrame test = new GUIFrame();
 		
 	}
 }
