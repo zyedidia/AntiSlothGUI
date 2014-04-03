ef098027ba46051da1bbc9cc66809ce9ff25e385
zyedidia <zyedidia@gmail.com>
2014-04-03 14:37:21 -0400

    Added stuff

---
+++ src/gui/GUIFrame.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class GUIFrame extends JFrame {
+	public GUIFrame() {
+		super("Anti-Sloth");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
